c29a690f1681519f005ae7e5e5c0d220fe24b814
geraldineDafa <43980690+dafageraldine@users.noreply.github.com>
2019-12-01 22:03:50 +0700

    Update Input_Pelanggan.py

--- Input_Pelanggan.py
+++ Input_Pelanggan.py
@@ -1,32 +1,75 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import (QLineEdit,QWidget,QApplication)
-import sys
-import pymysql
-import cv2
+from PyQt5.QtWidgets import (QLineEdit,QWidget,QApplication,QMessageBox)
+import sys,cv2,pymysql
+from PyQt5.QtGui import QPixmap
 import numpy as np
+from datetime import datetime
 
 class input(QWidget):
 	def takephoto(self):
-		cap = cv2.VideoCapture(0)
-		if cap.isOpened():
-			ret,frame = cap.read()
-		else:
-			ret = False
+		window =  QtWidgets.QMessageBox()
+		window.setWindowTitle("success")
+		window.setText("Foto Berhasil Disimpan")
+		cap = cv2.VideoCapture(1)
+		global c
+		global filename
 		while True:
-			cv2.imshow('camera',frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
+			check,frame = cap.read()
+			cv2.imshow("camera",frame)
+			cv2.namedWindow('camera',cv2.WINDOW_NORMAL)
+			cv2.resizeWindow("camera",300,300)
+			key = cv2.waitKey(1)
+			if key == ord('q'):
+				c = c+1
+				filename = "user" + str(c) + ".jpg"
+				cv2.imwrite(filename,frame)
 				break
 		cap.release()
+		cv2.destroyAllWindows()
+		window.exec_()
+
+	def messagebox(self,title,message):
+		mess = QtWidgets.QMessageBox()
+		mess.setWindowTitle(title)
+		mess.setText(message)
+		mess.setStandardButtons(QtWidgets.QMessageBox.Ok)
+		mess.exec_()
+
+	def flag(self):
+		ID = self.ID.text()
+		nama = self.nama.text()
+		alamat = self.alamat.text()
+		hp = self.hp.text()
+		if (ID == "" or nama == "" or alamat =="" or hp ==""):
+			self.tryagain()
+		else:
+			self.register()
+
+	def tryagain(self):
+		win = QtWidgets.QMessageBox()
+		win.setWindowTitle("GAGAL")
+		win.setText("Data Tidak Boleh Kosong")
+		win.setStandardButtons(QtWidgets.QMessageBox.Ok)
+		win.exec_()
 
 	def register(self):
 		ID = self.ID.text()
 		nama = self.nama.text()
 		alamat = self.alamat.text()
 		hp = self.hp.text()
+		now = datetime.now()
+		time = now.strftime("%d/%m/%Y %H:%M:%S")
 		conn = pymysql.connect(host="localhost",user="root",password="1234",database="inputpelanggan")
 		cur = conn.cursor()
-		query= ("insert into register(ID,nama,alamat,hp) values(%s,%s,%s,%s)")
-		data =cur.execute(query,(ID,nama,alamat,hp)) 
+		query= ("insert into register(ID,nama,alamat,hp,Time) values(%s,%s,%s,%s,%s)")
+		data =cur.execute(query,(ID,nama,alamat,hp,time))
+		conn.commit()
+		if (data):
+			self.messagebox("success","data berhasil di input")
+			self.ID.clear()
+			self.nama.clear()
+			self.alamat.clear()
+			self.hp.clear()
 
 	def __init__(self):
 		super().__init__()
@@ -51,12 +94,20 @@ class input(QWidget):
 
 		self.label_5 = QtWidgets.QLabel(self)
 		self.label_5.setText("Foto")
-		self.label_5.setGeometry(QtCore.QRect(30, 280, 47, 13))
+		self.label_5.setGeometry(QtCore.QRect(300, 100, 47, 13))
+		# global c
+		# global filename
+		# if (c > 0):
+		# 	self.label_6 = QLabel(self)
+		# 	self.label_6.setPixmap(QPixmap(filename))
+		# 	self.label_6.setGeometry(QtCore.QRect(300, 130,300, 260))
+		# else:
+		# 	False
 
 		self.pushbutton = QtWidgets.QPushButton(self)
 		self.pushbutton.setText("Input")
 		self.pushbutton.setGeometry(QtCore.QRect(140, 410, 75, 23))
-		self.pushbutton.clicked.connect(self.register)
+		self.pushbutton.clicked.connect(self.flag)
 
 		self.pushbutton2 = QtWidgets.QPushButton(self)
 		self.pushbutton2.setText("Ambil gambar")
@@ -83,7 +134,7 @@ class input(QWidget):
 		self.hp.setPlaceholderText("masukkan No HP Anda")
 		self.hp.setObjectName("hp")
 
-
+c=0
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	inputpelanggan = input()
