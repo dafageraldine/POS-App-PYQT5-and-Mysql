f6a867a0b4c9cc1b1b7cba75df57c71958a1473c
geraldineDafa <43980690+dafageraldine@users.noreply.github.com>
2019-12-22 20:04:03 +0700

    Delete clientbarcode.py

--- clientbarcode.py
+++
@@ -1,9 +0,0 @@
-import barcode,socket,random
-
-ip = '127.0.0.2'
-port = 1817
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((ip,port))
-while True:
-	code = s.recv(100)
-	print(code)
